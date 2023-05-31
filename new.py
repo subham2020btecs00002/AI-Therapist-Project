abeccde159ea946ed67535009810874240b6cfb2
subham2020btecs00002 <114304561+subham2020btecs00002@users.noreply.github.com>
2023-05-31 23:36:17 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,4 @@
+import os
+file_path = r'C:\Users\subha\Downloads\Aiproject\AI THETAPIST\intents.json'  
+file_size = os.path.getsize(file_path)
+print(f"The size of the file is {file_size} bytes.")
